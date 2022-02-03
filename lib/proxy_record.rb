e24e6ae632cab51b3b55846d6ba6d50ff92d8953
John Crepezzi <john.crepezzi@gmail.com>
2022-02-03 10:58:41 -0500

    Rename some methods

--- lib/proxy_record.rb
+++ lib/proxy_record.rb
@@ -5,27 +5,27 @@ module ProxyRecord
     Object
   end
 
-  def self.[](model_class)
-    ar_model_class = Class.new(model_class)
+  def self.[](data_model_parent_class)
+    data_model_class = Class.new(data_model_parent_class)
 
     klass = Class.new(Proxy)
-    klass.send(:model_class=, ar_model_class)
+    klass.send(:data_model_class=, data_model_class)
     klass
   end
 
   class Proxy
-    class_attribute :model_class, instance_predicate: false
-    private_class_method :model_class, :model_class=
+    class_attribute :data_model_class, instance_predicate: false
+    private_class_method :data_model_class, :data_model_class=
 
     def self.wrap(o)
       case o
-      when model_class then new(o)
+      when data_model_class then new(o)
       else raise 'Cannot wrap'
       end
     end
 
-    def self.model(&block)
-      model_class.class_eval(&block)
+    def self.data_model_eval(&block)
+      data_model_class.class_eval(&block)
     end
 
     class << self
--- spec/proxy_record_spec.rb
+++ spec/proxy_record_spec.rb
@@ -21,7 +21,7 @@ describe ProxyRecord do
 
     klass = Class.new(ProxyRecord[model_parent_class]) do
       def self.foo_proxy
-        model_class.foo
+        data_model_class.foo
       end
     end
 
@@ -36,7 +36,7 @@ describe ProxyRecord do
 
     klass = Class.new(ProxyRecord[model_parent_class])
 
-    instance = klass.wrap(klass.send(:model_class).new)
+    instance = klass.wrap(klass.send(:data_model_class).new)
 
     expect(instance).not_to respond_to(:foo)
   end
@@ -54,20 +54,20 @@ describe ProxyRecord do
       end
     end
 
-    instance = klass.wrap(klass.send(:model_class).new)
+    instance = klass.wrap(klass.send(:data_model_class).new)
 
     expect(instance.foo_proxy).to eq('result')
   end
 
   it 'should be able to run a model block for an AR::Base subclass' do
     klass = Class.new(ProxyRecord[ActiveRecord::Base]) do
-      model do
+      data_model_eval do
         self.table_name = 'users'
       end
 
       class << self
         def total_count
-          model_class.count
+          data_model_class.count
         end
       end
     end
@@ -77,13 +77,13 @@ describe ProxyRecord do
 
   it 'should be able to create & wrap an individual record' do
     klass = Class.new(ProxyRecord[ActiveRecord::Base]) do
-      model do
+      data_model_eval do
         self.table_name = 'users'
       end
 
       class << self
         def create
-          wrap(model_class.create(login: 'foo'))
+          wrap(data_model_class.create(login: 'foo'))
         end
       end
     end
@@ -91,14 +91,14 @@ describe ProxyRecord do
     expect(klass.create).to be_a(klass)
   end
 
-  it 'should not have public methods for accessing model_class or model' do
+  it 'should not have public methods for accessing data_model_class or model' do
     klass = Class.new(ProxyRecord[ActiveRecord::Base]) do
-      model do
+      data_model_eval do
         self.table_name = 'users'
       end
     end
 
-    expect(klass).not_to respond_to(:model_class)
-    expect(klass.wrap(klass.send(:model_class).new).private_methods).to include(:data_model)
+    expect(klass).not_to respond_to(:data_model_class)
+    expect(klass.wrap(klass.send(:data_model_class).new).private_methods).to include(:data_model)
   end
 end
