1b1658af8b87f25bb9fb2b2048a1ec53ce531b61
John Crepezzi <john.crepezzi@gmail.com>
2022-03-30 21:38:26 -0400

    Add clarifying comments

--- lib/lite_record/querying.rb
+++ lib/lite_record/querying.rb
@@ -4,19 +4,24 @@ module LiteRecord
   module Querying
     private
 
+    # Returns the first record
     def first
       ProxyRecord.wrap(data_model_class.first)
     end
 
+    # Returns the last record
     def last
       ProxyRecord.wrap(data_model_class.last)
     end
 
+    # Returns a ProxyRecord::CollectionProxy with the given conditions
     def where(*where_attributes)
       ar_scope = data_model_class.where(*where_attributes)
       ProxyRecord::CollectionProxy.new(ar_scope)
     end
 
+    # Refines & returns a new ProxyRecord::CollectionProxy adding the given
+    # conditions to the current scope
     def refine_scope(scope, *where_attributes)
       ar_scope = scope.instance_variable_get(:@collection).where(*where_attributes)
       ProxyRecord::CollectionProxy.new(ar_scope)
