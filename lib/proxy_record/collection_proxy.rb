5fffd962863a3a31426c1a67601bbd10567508cc
John Crepezzi <john.crepezzi@gmail.com>
2022-03-30 22:47:19 -0400

    Add comment that got lost back

--- lib/proxy_record/collection_proxy.rb
+++ lib/proxy_record/collection_proxy.rb
@@ -1,4 +1,14 @@
 module ProxyRecord
+  # Represents an in-progress scope on a given model.
+  #
+  # Note that this object is MUCH more limited than AR::Relation. This is
+  # intentional. If we defined methods here like `destroy_all` they'd either:
+  #
+  # * be private and thus not be callable from anywhere useful
+  # * be public and defeat the purpose of this library
+  #
+  # We have to treat this class as if it could be exported to another place
+  # by return and be used (it likely will).
   class CollectionProxy
     def initialize(collection)
       @collection = collection
